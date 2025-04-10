2bcd376408ae6da1799482f8ae0bc85405050683
TuanAnhCao <141569750+TuanAnhCao@users.noreply.github.com>
2025-04-11 01:14:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # Cấu hình bot
-TOKEN = os.environ.get("TELEGRAM_TOKEN", "7815604030:AAEsD_IaiMq8e1tFLi7FESZIeC_Uvlul4_E")  # Lấy token từ biến môi trường
+TOKEN = os.environ.get("TELEGRAM_TOKEN", "7815604030:AAF_EATALPeZpET4bOkkyeTFgBWUBDAGcHw")  # Lấy token từ biến môi trường
 ADMIN_ID = 6283529520  # Thay bằng Telegram ID của admin
 
 # Tạo session với retry
